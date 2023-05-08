8f70aabe3818cab60f8b427c19726f566092a16a
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2023-05-08 15:10:42 +0200

    register activity

--- app/src/main/java/com/example/progettoprogrammazionemobile/activity/LoginActivity.kt
+++ app/src/main/java/com/example/progettoprogrammazionemobile/activity/LoginActivity.kt
@@ -1,7 +1,8 @@
-package com.example.progettoprogrammazionemobile
+package com.example.progettoprogrammazionemobile.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.progettoprogrammazionemobile.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
