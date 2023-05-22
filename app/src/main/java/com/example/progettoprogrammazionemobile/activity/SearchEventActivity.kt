5f49e5469d6894426c70af8334f09314e729c478
AlessioGiacconi <84578962+AlessioGiacconi@users.noreply.github.com>
2023-05-22 12:33:29 +0200

    inizio search activity

---
+++ app/src/main/java/com/example/progettoprogrammazionemobile/activity/SearchEventActivity.kt
@@ -0,0 +1,12 @@
+package com.example.progettoprogrammazionemobile.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.progettoprogrammazionemobile.R
+
+class SearchEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_event)
+    }
+}
